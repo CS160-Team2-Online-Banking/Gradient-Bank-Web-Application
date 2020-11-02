6f82418ab7b500875b4499fac965a79a9bead16d
ZackFiner <zackary.finer@yahoo.com>
2020-11-02 01:28:14 -0800

    final commit for now
    i think this approach to check OCR could be viable, but i think implementing the other features of the banking application is more important for the time being.

--- bankapi/checkscanner/checkscanner.py
+++ bankapi/checkscanner/checkscanner.py
@@ -1,24 +1,53 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+""" # until this is complete, this file should remain commented out
 import pytesseract
-
-# convert the image of the check to grayscale
-# run it through tesseract OCR to generate a string
-# retrieve the output of the OCR and retrieve the account and routing number
-#
-
-
-pytesseract.pytesseract.tesseract_cmd = r'tesseract-4.0.0-alpha/tesseract.exe'
+from PIL import Image
+import requests
+from io import BytesIO
+import cv2
+import numpy as np
 
 
-def get_check_str(img):
-    gray_scale = img.convert('LA')
-    raw_check_str = pytesseract.image_to_string(gray_scale, lang='eng')
-    return raw_check_str
+def get_bottom_line(rects):
+    buckets = list()
+    for rect in rects:
+        match_found = False
+        rect_min = rect[1]
+        rect_max = rect[1] + rect[3]
+        for i, bucket in enumerate(buckets):  # check the buckets
+            bucket_min = bucket[0]
+            bucket_max = bucket[1]
+            if rect_min < bucket_max and rect_max > bucket_min:
+                buckets[i][0] = min(rect_min, bucket[0])
+                buckets[i][1] = max(rect_max, bucket[1])
+                buckets[i][2].append(rect)
+                match_found = True
+                break
+        if not match_found:  # add a new bucket if we couldn't find one
+            new_bucket = [rect_min, rect_max, [rect]]
+            buckets.append(new_bucket)
 
-def get_check_data(img):
-    raw_check_str = get_check_str(img)
+    sorted_buckets = sorted(buckets, key=(lambda x: x[0]), reverse=True)
+    return sorted_buckets[0]
 
 
+def get_micr_img(check_img):
+    # we will operate under the assmuption that the micr is the last clear thing
+    # on the check
+    # and the check is oriented relatively level with the camera
+    s = check_img.shape
+    micr_img = check_img[int(s[0] * 0.80):s[0], 0:s[1]]
+
+    ret, thresh = cv2.threshold(micr_img, 50, 255, 0)
+    ret, thresh2 = cv2.threshold(micr_img, 127, 255, 0)
+    cnts, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    bounding_rects = [cv2.boundingRect(c) for c in cnts]
+    bounding_rects = list(filter((lambda x: x[0] and x[1]), bounding_rects))
+    bottom_line = get_bottom_line(bounding_rects)
+    left = min(map(lambda x: x[0], bottom_line[2])) - 20
+    right = max(map(lambda x: x[0] + x[2], bottom_line[2])) + 20
+    top = bottom_line[0] - 5
+    bottom = bottom_line[1] + 5
+    micr_img = thresh[top:bottom, left:right]
+    return micr_img
+"""
