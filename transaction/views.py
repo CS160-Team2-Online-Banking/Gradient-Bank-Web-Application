84f605e10a159f384a8bc8ec31dd633e039b6220
yutakihara <k.kyuta.821@gmail.com>
2020-11-12 21:46:12 +0900

    modify the frontend for transaction page

--- transaction/views.py
+++ transaction/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from .utils import request_api
 
 
 class Transaction(View):
     def get(self, request, *args, **kwargs):
+        return render(request, 'transaction/transaction.html')
+
+    def post(self, request, *args, **kwargs):
+
+        form_values = [
+            self.request.POST.get('accounts', None),
+            self.request.POST.get('bills', None),
+            self.request.POST.get('amount', None),
+            self.request.POST.get('auto_payment_date', None),
+        ]
+        print(form_values)
         return render(request, 'transaction/transaction.html')
 
 
