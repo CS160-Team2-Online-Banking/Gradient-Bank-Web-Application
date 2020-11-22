33b69730c8abe6bdd8d9dcd9034e63c3ae9aaf01
ZackFiner <zackary.finer@yahoo.com>
2020-11-21 18:55:43 -0800

    Update urls.py

--- bankapi/urls.py
+++ bankapi/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path, re_path
-from bankapi.views import TransferView, AutoPaymentView, TransactionView, AccountView, AuthView, ReportView
+from bankapi.views import TransferView, AutoPaymentView, TransactionView, AccountView, ReportView  #, AuthView
 
 urlpatterns = [
     path('transfers', TransferView.as_view()),
-    path('authentication', AuthView.as_view()),
+    #path('authentication', AuthView.as_view()),
     path('autopayments', AutoPaymentView.as_post_view()),
     re_path(r'^autopayments/delete/(?P<autopayment_id>[0-9]+)?', AutoPaymentView.as_delete_view()),
     re_path(r'^autopayments/(?P<autopayment_id>[0-9]+)?', AutoPaymentView.as_access_view()),
